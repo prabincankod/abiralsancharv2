a5f8de0b187799784311a3909a80b27198a15e0a
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-12-26 22:22:00 +0545

    init bare minimum to get started

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+
+import "~/styles/globals.css";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+     <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+};
+
+export default MyApp;
